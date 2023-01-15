fffeacee5e23eb2e546a5d6514e54c63735ee097
Anthony Thibodeaux <actninswitch@gmail.com>
2023-01-15 10:47:20 -0700

    added process.env

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@
 const { Client } = require('pg'); // imports the pg module
 
 // supply the db name and location of the database
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL);
 
 async function getAllUsers() {
   const { rows } = await client.query(
